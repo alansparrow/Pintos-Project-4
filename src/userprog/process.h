e7f60f88f180a78c093f59796545a1da1e88a6c7
Sambasevam Shanmugam <sambasevam@gmail.com>
2012-10-26 00:59:58 -0700

    Rearranged for tidyness

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,6 +2,13 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+int process_add_file (struct file *f);
+struct file* process_get_file (int fd);
+struct process_file {
+  struct file *file;
+  int fd;
+  struct list_elem elem;
+};
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
